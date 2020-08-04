9ca8494ed7916f2b7f90bcb2302385821b9966a6
LeviWachira <trojan.wachira@gmail.com>
2020-08-04 16:53:22 +0700

    update gitignore

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,14 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  
+  apiKey: "AIzaSyDq3_ZO-Vh0Kr418x_1FXa2Zuicq4Pwig0",
+  authDomain: "facebook-messager-clone-react.firebaseapp.com",
+  databaseURL: "https://facebook-messager-clone-react.firebaseio.com",
+  projectId: "facebook-messager-clone-react",
+  storageBucket: "facebook-messager-clone-react.appspot.com",
+  messagingSenderId: "696389906917",
+  appId: "1:696389906917:web:a3c4450e5688b281f47f0c",
+  measurementId: "G-ZQDLQE0CN1",
 });
 
 
