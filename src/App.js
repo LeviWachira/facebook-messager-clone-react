0707b39e72c813bdf9a9fa4369ffec19e9f2748e
LeviWachira <trojan.wachira@gmail.com>
2020-08-04 16:42:47 +0700

    finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Button, FormControl, Input, InputLabel } from "@material-ui/core";
+import firebase from "firebase";
+import FlipMove from "react-flip-move";
+import SendIcon from "@material-ui/icons/Send";
+import { IconButton } from "@material-ui/core";
+
+import Message from "./Message";
+import db from "./firebase";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    db.collection("messages")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setMessages(
+          snapshot.docs.map((doc) => ({ id: doc.id, message: doc.data() }))
+        );
+      });
+  }, []);
+
+  useEffect(() => {
+    setUsername(prompt("Please enter your name"));
+  }, []);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    db.collection("messages").add({
+      message: input,
+      username: username,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    // setMessages([...messages, { username: username, text: input }]);
+    setInput("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img
+        style={{ height: 100, widows: 100, marginBottom: -10, marginTop: 30 }}
+        src="https://img.pngio.com/messenger-facebook-messenger-icon-free-png-images-clipart-messenger-icon-png-920_981.png"
+      />
+      <h1>Messager</h1>
+      <h2>Welcome {username}</h2>
+
+      <form className="app__form">
+        <FormControl className="app__formControl">
+          <Input
+            className="app__input"
+            placeholder="Enter a message..."
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <IconButton
+            className="app__iconButton"
+            variant="contained"
+            color="primary"
+            type="submit"
+            disabled={!input}
+            onClick={sendMessage}
+          >
+            <SendIcon />
+          </IconButton>
+        </FormControl>
+      </form>
+
+      <FlipMove>
+        {messages.map(({ id, message }) => (
+          <Message key={id} username={username} message={message} />
+        ))}
+      </FlipMove>
     </div>
   );
 }
